4204b17ac9628e4c1c0cc8b74018a3a8f814c600
Mitra-Electronics <ishanmitra020@gmail.com>
2023-08-04 19:58:06 +0530

    v0.0.1 linted

--- drivers/db.py
+++ drivers/db.py
@@ -2,8 +2,8 @@
 from pathlib import Path
 from sqlmodel import create_engine
 
-# CONNSTR = f'postgresql://{getenv("NEON_USERNAME")}:{getenv("NEON_PASSWD")}@
-# {getenv("NEON_CLUSTER")}/{getenv("NEON_DB_NAME")}'
+# CONNSTR = f'postgresql://{getenv("NEON_USERNAME")}:{getenv("NEON_PASSWD")}
+# @{getenv("NEON_CLUSTER")}/{getenv("NEON_DB_NAME")}'
 BASE_DIR = Path(__file__).parent.parent
 CONNSTR = f"sqlite:///{BASE_DIR/ 'db.db'}"
 
