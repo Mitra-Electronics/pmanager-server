1efe029bafec32f023a4eabe27a3659c55a3d0e1
Mitra-Electronics <ishanmitra020@gmail.com>
2023-08-30 15:14:40 +0530

    Added tests for auth

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,8 +1,8 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import SQLModel, Session, create_engine
-from src.drivers.db import get_session
-from src.main import app
+from ..src.drivers.db import get_session
+from ..src.main import app
 
 
 @pytest.fixture(name="session")
@@ -25,3 +25,77 @@ def client_fixture(session: Session):
     client = TestClient(app)
     yield client
     app.dependency_overrides.clear()
+
+
+def test_register_unprocessable_user(client: TestClient):
+    response = client.post(
+        "/register/",
+        json={
+            "email": "user@example.com",
+            "password": "123"
+        }
+    )
+    assert response.status_code == 422
+
+
+def test_register_empty_user(client: TestClient):
+    response = client.post(
+        "/register/",
+        json={
+            "email": "user@example.com",
+            "password": "123",
+            "first_name": "",
+            "last_name": "",
+            "country": "India"
+        }
+    )
+    assert response.status_code == 422
+
+
+def test_register_user(client: TestClient):
+    response = client.post(
+        "/register/",
+        json={
+            "email": "user@example.com",
+            "password": "123",
+            "first_name": "Example",
+            "last_name": "Surname",
+            "country": "India"
+        }
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content["success"] is True
+
+
+def test_login_user_wrong_email(client: TestClient):
+    response = client.post(
+        "/login/",
+        json={
+            "email": "usernone@example.com",
+            "password": "123"
+        }
+    )
+    assert response.status_code == 401
+
+
+def test_login_user_wrong_password(client: TestClient):
+    response = client.post(
+        "/login/",
+        json={
+            "email": "user@example.com",
+            "password": "ioj"
+        }
+    )
+    assert response.status_code == 401
+
+
+def test_login_user_right_password(client: TestClient):
+    response = client.post(
+        "/login/",
+        json={
+            "email": "user@example.com",
+            "password": "123"
+        }
+    )
+    assert response.status_code == 200
